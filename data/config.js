30285d5e3777e593d127e93b491ab4c02952e792
unknown <andremm@outlook.pt>
2022-09-05 09:47:52 -0300

    update:portifolio

---
+++ data/config.js
@@ -0,0 +1,62 @@
+export const meta = {
+  title: 'André Martins - Web Developer',
+  description: 'Hey, I\'m André. I\'m a web developer and designer building things people love.'
+}
+
+export const hero = {
+  title: "Eaii, aqui quem fala é o André.",
+  desc: "Sou desenvolvedor back-end (porém muito bem treinado em front) que ama tecnologia e as facilidades que dispõe!"
+};
+
+export const projects = {
+  title: 'Meus projetos',
+  desc: "Aqui está tudo que ja trabalhei ou que venho trabalhando nos últimos tempos.",
+  projects: [
+    {
+      title: 'Em breve!',
+      description: 'Ainda estou desenvolvendo esta sessão',
+      link: '',
+      github: ''
+    },
+    {
+      title: 'Em breve!',
+      description: 'Ainda estou desenvolvendo esta sessão',
+      link: '',
+      github: ''
+    },
+    {
+      title: 'Em breve!',
+      description: 'Ainda estou desenvolvendo esta sessão ',
+      link: '',
+      github: ''
+    }
+  ]
+};
+
+export const stack = {
+  title: 'Stacks',
+  stack: [
+    'Python',
+    'C/C++',
+    'Angular',
+    'Typescript',
+    'Docker',
+    'Linux',
+    'NodeJS',
+    'MongoDB',
+    'SCRUM',
+    'SQL',
+    'ReactJS',
+    'NextJS',
+    'PHP',
+    'Git'
+  ]
+};
+
+export const contact = {
+  title: 'Entre em contato',
+  email: 'andremm@outlook.pt',
+  github: 'mrtnsandre',
+  dribbble: '',
+  linkedin: 'mrtnsandre'
+};
